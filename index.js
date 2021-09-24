01f05a5d779fedb1727a87e53095a54a71298cec
Michael Barrett <mike182uk@gmail.com>
2021-09-24 14:07:05 +0100

    add jsdoc

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ const replacePlaylistTracksTask = require('./lib/task/replacePlaylistTracks')
 const shuffleTracksTask = require('./lib/task/shuffleTracks')
 const sortTracksTask = require('./lib/task/sortTracks')
 
+/**
+ * Log an error message to the console. Also log the original error to the console
+ * if the APP_ENV environment variable is set to "dev"
+ *
+ * @param {string} message
+ * @param {Error} error
+ *
+ * @returns {void}
+ */
 function logErr (message, error) {
   console.error(chalk.red(message))
 
@@ -50,16 +59,37 @@ function logErr (message, error) {
   }
 }
 
+/**
+ * Log a warning message to the console
+ *
+ * @param {string} message
+ *
+ * @returns {void}
+ */
 function logWarn (message) {
   console.warn(chalk.yellow(message))
 }
 
+/**
+ * Log an info message to the console
+ *
+ * @param {string} message
+ *
+ * @returns {void}
+ */
 function logInfo (message) {
   if (process.env.DEBUG !== '') return
 
   console.info(chalk.blue(message))
 }
 
+/**
+ * Load config from either the provided config path or stdin
+ *
+ * @param {string} [configPath]
+ *
+ * @returns {Promise<object>}
+ */
 async function loadConfig (configPath) {
   let config
 
@@ -88,6 +118,13 @@ async function loadConfig (configPath) {
   return config
 }
 
+/**
+ * Validate that the provided config is valid
+ *
+ * @param {object} config
+ *
+ * @returns {void}
+ */
 function validateConfig (config) {
   const schema = buildSchema([
     dedupeTracksTask,
@@ -107,6 +144,181 @@ function validateConfig (config) {
   }
 }
 
+/**
+ * Attempt to authenticate with Spotify using: Authorization Code Flow with Proof Key for Code Exchange (PKCE)
+ *
+ * @see https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow-with-proof-key-for-code-exchange-pkce
+ *
+ * @returns {Promise<object>}
+ */
+async function authenticate () {
+  const codeVerifierSize = Math.random() * (128 - 43 + 1) + 43 //  43 = min, 128 = max
+  const codeVerifier = crypto.randomBytes(codeVerifierSize)
+    .toString('hex')
+    .slice(0, codeVerifierSize)
+  const codeChallenge = crypto.createHash('sha256')
+    .update(codeVerifier)
+    .digest()
+    .toString('base64url')
+  const state = crypto.randomBytes(32).toString('hex')
+  const url = createAuthorizationURL({
+    redirectUri: SPOTIFY_APP_REDIRECT_URI,
+    clientId: SPOTIFY_APP_CLIENT_ID,
+    scopes: SPOTIFY_APP_REQUIRED_SCOPES,
+    state,
+    codeChallenge
+  })
+
+  // Prompt user to visit authentication URL
+  logWarn(`\nAuthorization required. Please visit the following URL in a browser on this machine:\n\n${url}\n`)
+
+  // Start a HTTP server that is listening on the same URI that the spotify app
+  // is set to redirect too after user accepts / declines authentication. Wait for
+  // a request to the server before continuing execution
+  let server
+
+  const result = await new Promise((resolve, reject) => {
+    server = http.createServer(async (req, res) => {
+      debugAuth('HTTP request received to authentication handler')
+
+      const queryParams = new URL(req.url, SPOTIFY_APP_REDIRECT_URI).searchParams
+
+      if (queryParams.get('state') !== state) {
+        res.writeHead(500)
+        res.end('Authentication unsuccessful')
+
+        return reject(new Error('Authentication failed due to: Invalid state received to authentication HTTP handler'))
+      } else {
+        const { body, statusCode } = await getAccessToken({
+          redirectUri: SPOTIFY_APP_REDIRECT_URI,
+          clientId: SPOTIFY_APP_CLIENT_ID,
+          code: queryParams.get('code'),
+          codeVerifier
+        })
+
+        if (statusCode !== 200) {
+          res.writeHead(500)
+          res.end('Authentication unsuccessful')
+
+          return reject(
+            new Error(`Authentication failed due to: Invalid status code [${statusCode}] received whilst requesting access token`)
+          )
+        }
+
+        res.writeHead(200)
+        res.end('Authentication successful! You can now close this page')
+
+        return resolve({
+          accessToken: body.access_token,
+          refreshToken: body.refresh_token,
+          expiresAt: new Date(new Date().getTime() + (1000 * body.expires_in))
+        })
+      }
+    })
+
+    debugAuth('Starting HTTP authentication handler')
+
+    server.listen(SPOTIFY_APP_REDIRECT_URI_PORT)
+  })
+
+  // After a HTTP request has been received to the server, close it and return
+  // the result
+  server.close()
+
+  return result
+}
+
+/**
+ * Initialise the Spotify client ensuring that it is authenticated to make requests
+ *
+ * @param {Conf} appData
+ *
+ * @returns {Promise<SpotifyWebApi>}
+ */
+async function initSpotify (appData) {
+  const spotify = new SpotifyWebApi({
+    clientId: SPOTIFY_APP_CLIENT_ID,
+    redirectUri: SPOTIFY_APP_REDIRECT_URI
+  })
+
+  // If an access token is present in environment use it for authenticating requests
+  // otherwise attempt authentication flow
+  if (process.env.ACCESS_TOKEN !== undefined) {
+    debugAuth('Using access token provided in environment for authentication')
+
+    spotify.setAccessToken(process.env.ACCESS_TOKEN)
+  } else {
+    debugAuth('Attempting authentication flow')
+
+    const existingSpotifyAuthData = appData.get(APP_DATA_SPOTIFY_AUTH_KEY)
+
+    // If user has not previously authenticated, attempt authentication
+    if (existingSpotifyAuthData === undefined) {
+      debugAuth('Authentication data not present in app data')
+
+      const { accessToken, refreshToken, expiresAt } = await authenticate()
+
+      appData.set(APP_DATA_SPOTIFY_AUTH_KEY, { accessToken, refreshToken, expiresAt })
+
+      spotify.setAccessToken(accessToken)
+      spotify.setRefreshToken(refreshToken)
+    } else {
+      // If user has previously authenticated, check that authentication credentials
+      // are still valid
+      debugAuth('Authentication data present in app data, skipping authentication flow')
+
+      spotify.setAccessToken(existingSpotifyAuthData.accessToken)
+      spotify.setRefreshToken(existingSpotifyAuthData.refreshToken)
+
+      // If the existing authentication credentials have expired, or are due to
+      // expire within the next minute, request refreshed credentials
+      if (
+        new Date(existingSpotifyAuthData.expiresAt) <=
+        new Date(new Date().getTime() - (1000 * 60))
+      ) {
+        try {
+          debugAuth('Access token expired, refreshing access token')
+
+          const { body } = await getRefreshedAccessToken({
+            refreshToken: spotify.getRefreshToken(),
+            clientId: spotify.getClientId()
+          })
+
+          appData.set(APP_DATA_SPOTIFY_AUTH_KEY, {
+            accessToken: body.access_token,
+            expiresAt: new Date(new Date().getTime() + (1000 * body.expires_in)),
+            refreshToken: body.refresh_token
+          })
+
+          spotify.setAccessToken(body.access_token)
+          spotify.setRefreshToken(body.refresh_token)
+        } catch (err) {
+          // If an error occurs during refreshing authentication credentials, clear
+          // any saved authentication credentials to force user to re-authenticate
+          // on next execution
+          appData.delete(APP_DATA_SPOTIFY_AUTH_KEY)
+
+          throw new Error('An authentication error has occurred. Re-authentication is needed, please try running the command again')
+        }
+      } else {
+        debugAuth('Access token has not yet expired, refresh not required')
+      }
+    }
+  }
+
+  debugAuth('Authentication successful')
+
+  return spotify
+}
+
+/**
+ * Execute all tasks listed in the config
+ *
+ * @param {object} config
+ * @param {SpotifyWebApi} spotify
+ *
+ * @returns {Promise<void>}
+ */
 async function executeTasks (config, spotify) {
   const taskIds = Object.keys(config.tasks)
   const trackCollections = {}
@@ -186,161 +398,13 @@ async function executeTasks (config, spotify) {
   logInfo('All tasks executed!')
 }
 
-async function authenticate () {
-  // Attempt to authentication using: Authorization Code Flow with Proof Key for Code Exchange (PKCE)
-  // See docs for further details - https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow-with-proof-key-for-code-exchange-pkce
-  const codeVerifierSize = Math.random() * (128 - 43 + 1) + 43 //  43 = min, 128 = max
-  const codeVerifier = crypto.randomBytes(codeVerifierSize)
-    .toString('hex')
-    .slice(0, codeVerifierSize)
-  const codeChallenge = crypto.createHash('sha256')
-    .update(codeVerifier)
-    .digest()
-    .toString('base64url')
-  const state = crypto.randomBytes(32).toString('hex')
-  const url = createAuthorizationURL({
-    redirectUri: SPOTIFY_APP_REDIRECT_URI,
-    clientId: SPOTIFY_APP_CLIENT_ID,
-    scopes: SPOTIFY_APP_REQUIRED_SCOPES,
-    state,
-    codeChallenge
-  })
-
-  // Prompt user to visit authentication URL
-  logWarn(`\nAuthorization required. Please visit the following URL in a browser on this machine:\n\n${url}\n`)
-
-  // Start a HTTP server that is listening on the same URI that the spotify app
-  // is set to redirect too after user accepts / declines authentication. Wait for
-  // a request to the server before continuing execution
-  let server
-
-  const result = await new Promise((resolve, reject) => {
-    server = http.createServer(async (req, res) => {
-      debugAuth('HTTP request received to authentication handler')
-
-      const queryParams = new URL(req.url, SPOTIFY_APP_REDIRECT_URI).searchParams
-
-      if (queryParams.get('state') !== state) {
-        res.writeHead(500)
-        res.end('Authentication unsuccessful')
-
-        return reject(new Error('Authentication failed due to: Invalid state received to authentication HTTP handler'))
-      } else {
-        const { body, statusCode } = await getAccessToken({
-          redirectUri: SPOTIFY_APP_REDIRECT_URI,
-          clientId: SPOTIFY_APP_CLIENT_ID,
-          code: queryParams.get('code'),
-          codeVerifier
-        })
-
-        if (statusCode !== 200) {
-          res.writeHead(500)
-          res.end('Authentication unsuccessful')
-
-          return reject(
-            new Error(`Authentication failed due to: Invalid status code [${statusCode}] received whilst requesting access token`)
-          )
-        }
-
-        res.writeHead(200)
-        res.end('Authentication successful! You can now close this page')
-
-        return resolve({
-          accessToken: body.access_token,
-          refreshToken: body.refresh_token,
-          expiresAt: new Date(new Date().getTime() + (1000 * body.expires_in))
-        })
-      }
-    })
-
-    debugAuth('Starting HTTP authentication handler')
-
-    server.listen(SPOTIFY_APP_REDIRECT_URI_PORT)
-  })
-
-  // After a HTTP request has been received to the server, close it and return
-  // the result
-  server.close()
-
-  return result
-}
-
-async function initSpotify (appData) {
-  const spotify = new SpotifyWebApi({
-    clientId: SPOTIFY_APP_CLIENT_ID,
-    redirectUri: SPOTIFY_APP_REDIRECT_URI
-  })
-
-  // If an access token is present in environment use it for authenticating requests
-  // otherwise attempt authentication flow
-  if (process.env.ACCESS_TOKEN !== undefined) {
-    debugAuth('Using access token provided in environment for authentication')
-
-    spotify.setAccessToken(process.env.ACCESS_TOKEN)
-  } else {
-    debugAuth('Attempting authentication flow')
-
-    const existingSpotifyAuthData = appData.get(APP_DATA_SPOTIFY_AUTH_KEY)
-
-    // If user has not previously authenticated, attempt authentication
-    if (existingSpotifyAuthData === undefined) {
-      debugAuth('Authentication data not present in app data')
-
-      const { accessToken, refreshToken, expiresAt } = await authenticate()
-
-      appData.set(APP_DATA_SPOTIFY_AUTH_KEY, { accessToken, refreshToken, expiresAt })
-
-      spotify.setAccessToken(accessToken)
-      spotify.setRefreshToken(refreshToken)
-    } else {
-      // If user has previously authenticated, check that authentication credentials
-      // are still valid
-      debugAuth('Authentication data present in app data, skipping authentication flow')
-
-      spotify.setAccessToken(existingSpotifyAuthData.accessToken)
-      spotify.setRefreshToken(existingSpotifyAuthData.refreshToken)
-
-      // If the existing authentication credentials have expired, or are due to
-      // expire within the next minute, request refreshed credentials
-      if (
-        new Date(existingSpotifyAuthData.expiresAt) <=
-        new Date(new Date().getTime() - (1000 * 60))
-      ) {
-        try {
-          debugAuth('Access token expired, refreshing access token')
-
-          const { body } = await getRefreshedAccessToken({
-            refreshToken: spotify.getRefreshToken(),
-            clientId: spotify.getClientId()
-          })
-
-          appData.set(APP_DATA_SPOTIFY_AUTH_KEY, {
-            accessToken: body.access_token,
-            expiresAt: new Date(new Date().getTime() + (1000 * body.expires_in)),
-            refreshToken: body.refresh_token
-          })
-
-          spotify.setAccessToken(body.access_token)
-          spotify.setRefreshToken(body.refresh_token)
-        } catch (err) {
-          // If an error occurs during refreshing authentication credentials, clear
-          // any saved authentication credentials to force user to re-authenticate
-          // on next execution
-          appData.delete(APP_DATA_SPOTIFY_AUTH_KEY)
-
-          throw new Error('An authentication error has occurred. Re-authentication is needed, please try running the command again')
-        }
-      } else {
-        debugAuth('Access token has not yet expired, refresh not required')
-      }
-    }
-  }
-
-  debugAuth('Authentication successful')
-
-  return spotify
-}
-
+/**
+ * Run the application
+ *
+ * @param {string} configPath
+ *
+ * @returns {Promise<void>}
+ */
 async function run (configPath) {
   try {
     // Initialise app data
